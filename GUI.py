e78f0f01f724cb6ada2dfeeb9c0cfd7047be4eca
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-02 10:27:06 -0600

    add text to text area

--- GUI.py
+++ GUI.py
@@ -1,19 +1,24 @@
 import tkinter as tk
 
 def button_clicked():
-    print("Button was clicked!")
+    # print("Button was clicked!")
+    outputArea.insert(tk.END, "Button was Clicked ")
 
 def showLocations():
-    print("Data would be here")
+    # print("Data would be here")
+    outputArea.insert(tk.END, "Data would be here ")
 
 root = tk.Tk()
 root.title("Button Example")
-root.geometry("300x200")
+root.geometry("400x300")
 
 button = tk.Button(root, text="Click Me!", command=button_clicked)
 showButton = tk.Button(root, text="Show Locations", command=showLocations)
+outputArea = tk.Text(root, height=10, width=40)
 
-button.pack(pady=20)
-showButton.pack(pady=20)
+
+button.pack(pady=2)
+showButton.pack(pady=2)
+outputArea.pack(pady=2)
 
 root.mainloop()
