c875ff39c8ef779afe93a3a42f10e8c549b6e415
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-02 10:15:19 -0600

    second button

--- GUI.py
+++ GUI.py
@@ -3,11 +3,15 @@ import tkinter as tk
 def button_clicked():
     print("Button was clicked!")
 
+def showLocations():
+    print("Data would be here")
+
 root = tk.Tk()
 root.title("Button Example")
 root.geometry("300x200")
 
 button = tk.Button(root, text="Click Me!", command=button_clicked)
+showButton = tk.Button(root, text="Show Locations", command=showLocations)
 
 button.pack(pady=50)
 
