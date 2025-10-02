618ed92d04eac9180ca05a071093871fd92d2893
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-02 10:43:17 -0600

    Add named label and its text

--- GUI.py
+++ GUI.py
@@ -7,7 +7,9 @@ def button_clicked():
 
 def showLocations():
     # print("Data would be here")
+    # deletes top text?
     outputArea.delete(1.0, tk.END)
+    # puts specific text at bottom of text area
     outputArea.insert(tk.END, "Data would be here\n")
 
 root = tk.Tk()
@@ -17,10 +19,14 @@ root.geometry("400x300")
 button = tk.Button(root, text="Click Me!", command=button_clicked)
 showButton = tk.Button(root, text="Show Locations", command=showLocations)
 outputArea = tk.Text(root, height=10, width=40)
+labelId = tk.Label(root, text="Input Id")
+txtId = tk.Text(root)
 
 
 button.pack(pady=2)
 showButton.pack(pady=2)
 outputArea.pack(pady=2)
+labelId.pack(pady=2)
+txtId.pack(pady=2)
 
 root.mainloop()
