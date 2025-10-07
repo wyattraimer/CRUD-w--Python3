292d179aa05346062dc1ed32de2a271859b21de9
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-07 10:34:12 -0600

    more specific file for DB commands?

---
+++ DBGUI.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+
+def button_clicked():
+    global outputArea
+    global txtId
+    outputArea.delete(1.0, tk.END)
+    outputArea.insert(tk.END, "Button was Clicked\n" + txtId.get(1.0,tk.END))
+
+def showLocations():
+    global selected
+    global outputArea
+    # deletes top text?
+    outputArea.delete(1.0, tk.END)
+    # puts specific text at bottom of text area
+    outputArea.insert(tk.END, "Data would be here\n" + selected.get())
+
+root = tk.Tk()
+root.title("Button Example")
+root.geometry("500x350")
+
+root.grid_columnconfigure(0, weight=1, uniform="col")
+root.grid_columnconfigure(1, weight=1, uniform="col")
+
+button = tk.Button(root, text="Click Me!", command=button_clicked)
+showButton = tk.Button(root, text="Show Locations", command=showLocations)
+outputArea = tk.Text(root, height=10, width=40)
+labelId = tk.Label(root, text="Input Id")
+txtId = tk.Text(root, height=1)
+
+items = ["--Select--", "Wyatt", "was", "here"]
+label2Id = tk.Label(root, text="Drop Down Demo")
+selected=tk.StringVar(value="--Select--")
+lb=tk.OptionMenu(root, selected, *items)
+
+button.grid(row=0,column=0)
+showButton.grid(row=0,column=1)
+outputArea.grid(row=1,column=0,columnspan=2,sticky="ew")
+labelId.grid(row=2,column=0)
+txtId.grid(row=2,column=1)
+label2Id.grid(row=3,column=0)
+lb.grid(row=3,column=1,sticky="ew")
+
+root.mainloop()
