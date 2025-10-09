c0a7e4ee8a82992f29ada0eb67a5ac259eeaa844
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-09 09:49:28 -0600

    import DB file. delete add code. change save button command.

--- DBGUI.py
+++ DBGUI.py
@@ -1,10 +1,8 @@
 import tkinter as tk
+import DB import *
 
 def add_clicked():
-    global outputArea
-    global txtId
-    outputArea.delete(1.0, tk.END)
-    outputArea.insert(tk.END, "Button was Clicked\n" + txtId.get(1.0,tk.END))
+    
 
 def delete_clicked():
     global outputArea
@@ -20,12 +18,6 @@ def update_clicked():
     # puts specific text at bottom of text area
     outputArea.insert(tk.END, "Data would be here\n" + selected.get())
 
-def saveChange_clicked():
-    global outputArea
-    global txtId
-    outputArea.delete(1.0, tk.END)
-    outputArea.insert(tk.END, "Button was Clicked\n" + txtId.get(1.0,tk.END))
-
 root = tk.Tk()
 root.title("Location Data Tool")
 root.geometry("600x300")
@@ -37,7 +29,7 @@ root.grid_columnconfigure(3, weight=1, uniform="col")
 addButton = tk.Button(root, text="Add", command=add_clicked)
 deleteButton = tk.Button(root, text="Delete", command=delete_clicked)
 updateButton = tk.Button(root, text="Update", command=update_clicked)
-saveButton = tk.Button(root, text="Save Changes", command=saveChange_clicked)
+saveButton = tk.Button(root, text="Save Changes", command=commitChanges)
 outputArea = tk.Text(root, height=10, width=40)
 
 addButton.grid(row=5,column=0)
