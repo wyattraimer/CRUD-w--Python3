b043329bce1a3299e1e19423a769f67973cde2b4
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-16 10:05:38 -0600

    using class code to troubleshoot

--- DB.py
+++ DB.py
@@ -19,8 +19,24 @@ def getLocations():
 def putLocation(values):
     cursor.execute(f"insert into location (x,y) values ({values[0]},{values[1]})")
 
-# d is a dictionary that includes id and either x,y or both
-def putChange(d):
+# # d is a dictionary that includes id and either x,y or both
+# def putChange(d):
+#     rec_id, x, y = d
+#     first="update location set "
+#     id=d["id"]
+#     last=f" where id={id}"
+#     value1=""
+#     if "x" in d:
+#         x=d["x"]
+#         value1=f"x={x}"
+#     value2=""
+#     if "y" in d:
+#         value1=f"y={y}"
+#     if "x" in d and "y" in d:
+#         value1=value1+","
+#     cursor.execute(first+value1+value2+last)
+
+def putChange(d): # d is a dictinary that include id and either x, y or both
     first="update location set "
     id=d["id"]
     last=f" where id={id}"
@@ -30,8 +46,9 @@ def putChange(d):
         value1=f"x={x}"
     value2=""
     if "y" in d:
-        value1=f"y={y}"
-    if "x" in d and "y" in d:
+        y=d["y"]
+        value2=f"y={y}"
+    if ("x" in d) and ("y" in d):
         value1=value1+","
     cursor.execute(first+value1+value2+last)
 
