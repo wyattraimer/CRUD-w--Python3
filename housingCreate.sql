a2e496f212e508d858ef6cc917a51eeca2a5481a
Wyatt Raimer <wyattraimer@gmail.com>
2025-11-04 09:52:29 -0700

    to create table in postgres DB

---
+++ housingCreate.sql
@@ -0,0 +1,12 @@
+create table housing (
+longitude float,
+latitude float,
+housing_median_age float,
+total_rooms float,
+total_bedrooms float,
+population float,
+households float,
+median_income float,
+median_house_value float,
+ocean_proximity text
+);
