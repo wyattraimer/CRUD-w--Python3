65499858cb52d0e94b4d1831eb17f903173e17f0
Wyatt Raimer <wyattraimer@gmail.com>
2025-11-11 09:33:58 -0700

    class code

--- DBHousing.py
+++ DBHousing.py
@@ -5,30 +5,58 @@ import os
 
 load_dotenv()
 
-connection=psycopg2.connect(user="accounting", password=os.getenv("PASSWORD"), host="localhost", database="Fall2025CSCI260")
-
-cursor=connection.cursor()
+connection = psycopg2.connect(user="accounting",
+                              password=os.getenv("PASSWORD"),
+                              host="localhost",
+                              database="Fall2025CSCI260")
+cursor =connection.cursor() 
 
 def getLocations():
-    cursor.execute("select latitude, longitude from housing where ocean_proximity='NEAR BAY'")
+    cursor.execute("Select latitude,longitude,sqrt(power(longitude - -120.14,2)+power(latitude-34.6,2)) as d \
+                    from housing order by d asc limit 1000")
     data=cursor.fetchall()
     return data
 
-print(os.getenv("PASSWORD"))
+def getAgeVsValue():
+    cursor.execute("Select housing_median_age,median_house_value,abs(median_house_value - 241400.0)+abs(housing_median_age-52.0) as d \
+                    from housing order by d asc limit 100")
+    data=cursor.fetchall()
+    return data
+
+#print(os.getenv("PASSWORD")) 
 latsLongs=getLocations()
-# print(latsLongs)
+#print(latsLongs)
 
 lats=[]
 longs=[]
+for item in latsLongs:
+    lats.append(item[0])
+    longs.append(item[1])
 
-for ll in latsLongs:
-    lats.append(ll[0])
-    longs.append(ll[1])
+#plt.scatter(lats,longs)
 
-plt.scatter(lats, longs)
+#plt.xlabel('Latitude')
+#plt.ylabel('Longitude')
+#plt.title('Location Plot')
 
-plt.xlabel('Latitude')
-plt.ylabel('Longituide')
-plt.title('Location Plot')
+#plt.show()
+
+agesValues=getAgeVsValue()
+
+ages=[]
+values=[]
+for item in agesValues:
+    ages.append(item[0])
+    values.append(item[1])
+
+ageGood=[52.0]
+valueGood=[241400.0]
+
+plt.scatter(ages,values)
+plt.scatter(ageGood,valueGood,marker='s')
+
+plt.xlabel('Age')
+plt.ylabel('Value')
+plt.title('Age Vs Value Plot')
 
 plt.show()
